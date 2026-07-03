ea1af68b0a39ad5cdd6bcbc9fe6fda7ec6531fce
Tech Vion <tekvion@gmail.com>
2026-07-03 18:30:24 +0500

    widget.js route error resolved

--- routes/widget_routes.py
+++ routes/widget_routes.py
@@ -1,9 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory
 
 widget_bp = Blueprint('widget', __name__)
 
 @widget_bp.route('/widget.js')
 def serve_widget():
-    # Read the static JavaScript file from the project root
-    with open('widget.js', 'r') as f:
-        return f.read(), 200, {'Content-Type': 'application/javascript'}
+    # Serve the widget.js file from the static folder
+    return send_from_directory('static', 'widget.js')
